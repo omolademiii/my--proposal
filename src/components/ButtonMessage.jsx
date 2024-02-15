209be664e72c4b77664a4779e68de8fc122a55aa
omolade <tosin.ogunmola44@gmail.com>
2024-02-15 22:57:26 +0100

    correct

--- src/components/ButtonMessage.jsx
+++ src/components/ButtonMessage.jsx
@@ -4,7 +4,7 @@ function ButtonMessage() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <img src="https://media1.tenor.com/m/sJlcuXVA7V0AAAAC/1sec.gif" alt="GIF" />
-      <h1 className='font-bold'> ok yaaaay!!!</h1>
+      <h1 className='font-bold'> ok yaaaay!!!!</h1>
     </div>
   )
 }
